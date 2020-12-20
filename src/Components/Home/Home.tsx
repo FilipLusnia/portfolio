7394f90561a012620acca03e3dfa9b30cc4b1b0a
Filip Luśnia <uszaty223@onet.pl>
2020-12-20 11:42:31 +0100

    tweaks

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Hero from './Hero';
 import About from './About';
+import Showcase from './Showcase';
 
 function Home() {
   return (
     <>  
       <Hero/>
       <About/>
+      <Showcase/>
     </>
   );
 }
