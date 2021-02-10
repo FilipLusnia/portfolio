804df514daa7b554c11876a7e953cbd99b0b8e98
Filip Luśnia <uszaty223@onet.pl>
2021-02-10 23:05:11 +0100

    project details page mostly finished

--- src/Components/Contact/ContactTop.tsx
+++ src/Components/Contact/ContactTop.tsx
@@ -1,7 +1,7 @@
 function ContactTop() {
     return (
         <div className="top_container">
-            <h1>{'>'} NEED TO GET IN CONTACT WITH ME?</h1>
+            <h1>{'>'} NEED TO DISCUSS SOMETHING?</h1>
             <p>Whatever it is - send me a message. I'll reply as soon as possible!</p>
         </div>
     );
--- src/Components/Projects/ProjectDetails.tsx
+++ src/Components/Projects/ProjectDetails.tsx
@@ -9,45 +9,58 @@ interface projectParams {
     name: string,
     description: string,
     tech: {
-        frontend: Object[],
-        backend: Object[]
+        frontend: object[],
+        backend: object[]
     },
-    url: string
+    url: string,
+    photos:{
+        showcase:{
+            first: string,
+            second: string,
+            third: string
+        }
+    }
 }
 
 function ProjectDetails() {
     const { id } = useParams<RouteParams>();
     const [ selectedProject, setSelectedProject ] = useState<projectParams>();
-       
+
     useEffect(() => {
         fetch(`${process.env.REACT_APP_projects_endpoint}/${id}`)
         .then((resp) => resp.json())
         .then(data => setSelectedProject(data));
     }, [id]);
-    
-    return (
-        <div className="project_details_container">  
-            {selectedProject
-            ?   
-                <>
-                    <h1 className="project_details_name">{selectedProject.name}</h1>
-                    <p className="project_details_tech_title">Technologies</p>
-                    <div className="project_details_tech">
-                        {selectedProject.tech.frontend && 
-                            <p className="project_details_tech_frontend">
-                                <span>Frontend: </span>{selectedProject.tech.frontend}
-                            </p>
-                        }
 
-                        {selectedProject.tech.backend && 
-                            <p className="project_details_tech_backend">
-                                <span>Backend: </span>{selectedProject.tech.backend}
-                            </p>
-                        }
+    return (
+        <div className="project_showcase_container">  
+            {selectedProject
+            ?   <section className="project_showcase">
+                    <div className="project_details">
+                        <h1 className="project_details_name">{selectedProject.name}</h1>
+                        <p className="project_details_tech_title">Technologies</p>
+                        <div className="project_details_tech">
+                            {selectedProject.tech?.frontend && 
+                                <p className="project_details_tech_frontend">
+                                    <span>Frontend: </span>{selectedProject.tech.frontend}
+                                </p>
+                            }
+
+                            {selectedProject.tech?.backend && 
+                                <p className="project_details_tech_backend">
+                                    <span>Backend: </span>{selectedProject.tech.backend}
+                                </p>
+                            }
+                        </div>
+                        <p className="project_details_description">{selectedProject.description}</p>
+                        <a className="project_details_link" href={selectedProject.url} target='blank' rel="noreferrer" aria-label="Go to project's demo">See it live</a>
                     </div>
-                    <p className="project_details_description">{selectedProject.description}</p>
-                    <a className="project_details_link" href={selectedProject.url} target='blank' rel="noreferrer" aria-label="Go to project's demo">See it live</a>
-                </>
+                    <div className="project_snapshots">
+                        <img src={selectedProject.photos.showcase.third} className={selectedProject.photos.showcase.third ? 'snapshot -loaded' : 'snapshot'} alt='showcase_thumb'/>
+                        <img src={selectedProject.photos.showcase.second} className={selectedProject.photos.showcase.second ? 'snapshot -loaded' : 'snapshot'} alt='showcase_thumb'/>
+                        <img src={selectedProject.photos.showcase.first} className={selectedProject.photos.showcase.first ? 'snapshot -loaded' : 'snapshot'} alt='showcase_thumb'/>
+                    </div>
+                </section>
             :
                 <div className="project_details_fallback">
                     <p>Just a second. Really!</p>
