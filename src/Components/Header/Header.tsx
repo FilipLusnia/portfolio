fac0811ed784d6e185f73ffba48619d5a054d9ce
Filip Luśnia <uszaty223@onet.pl>
2021-04-19 17:50:42 +0200

    tweaked header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,21 +1,28 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { NavLink } from "react-router-dom";
 
 function Header() {
 
   const [isMenuActive, setIsMenuActive] = useState(false); 
   const [animationStop, setAnimationStop] = useState<any>({maxWidth: "60px"}); //prevents animation on page load
+  const [menuLinksStyle, setMenuLinksStyle] = useState<any>({display: 'none'}); //hides menu links when not visible
   const headerRef = useRef<any>(null);
 
-  const openMenu = () => {
-    setIsMenuActive(!isMenuActive);
-  };
-
   useEffect(() => {
-    // prevents menu from animating on page load ("fold" animation) and from opening before content is loaded
+    // prevents menu from animating on page load ("fold" animation)
     setTimeout(()=> {
       setAnimationStop(null) ;
     }, 600);
+
+    //hides menu links when not visible
+    let timeout;
+    !isMenuActive ?
+      timeout = setTimeout(() => { 
+        setMenuLinksStyle({display: 'none'});
+      }, 600)
+    :
+      clearTimeout(timeout);
+      setMenuLinksStyle({display: 'block'});
 
     const handleClick = (e: MouseEvent) => {
       e.preventDefault();
@@ -34,7 +41,7 @@ function Header() {
       <div ref={headerRef} className={isMenuActive ? "header -active" : "header"} style={animationStop}> 
         
         <div className="nav_items">
-          <div onClick={openMenu} className={isMenuActive ? "hamburger -active" : "hamburger"}>
+          <div onClick={() => setIsMenuActive(!isMenuActive)} className={isMenuActive ? "hamburger -active" : "hamburger"}>
             <div className="hamburger-box">
               <div className="hamburger-inner"/>
             </div>
@@ -44,6 +51,7 @@ function Header() {
             exact to="/" 
             activeClassName="-current" 
             className={isMenuActive ? "nav_item -first -active" : "nav_item -first"} 
+            style={menuLinksStyle}
             onClick={()=> setIsMenuActive(false)}>
               HOME
           </NavLink>
@@ -52,6 +60,7 @@ function Header() {
             exact to="/projects" 
             activeClassName="-current" 
             className={isMenuActive ? "nav_item -second -active" : "nav_item -second"} 
+            style={menuLinksStyle}
             onClick={()=> setIsMenuActive(false)}>
               PROJECTS
           </NavLink>
@@ -59,7 +68,8 @@ function Header() {
           <NavLink 
             exact to="/contact" 
             activeClassName="-current" 
-            className={isMenuActive ? "nav_item -third -active" : "nav_item -third"} 
+            className={isMenuActive ? "nav_item -third -active" : "nav_item -third"}
+            style={menuLinksStyle}
             onClick={()=> setIsMenuActive(false)}>
               CONTACT
           </NavLink>
--- src/Components/ScrollToTop.ts
+++ src/Components/ScrollToTop.ts
@@ -1,4 +1,4 @@
-import {useEffect} from "react";
+import { useEffect } from "react";
 import { withRouter } from "react-router-dom";
 
 function ScrollToTop(props: any){
