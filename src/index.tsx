321b762d90a2ac2bf92cf1a85ef5e369437e13da
Filip Luśnia <uszaty223@onet.pl>
2021-05-14 19:18:24 +0200

    small fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
 import './Styles/main.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
